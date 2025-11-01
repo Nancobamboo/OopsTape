0fdcee8ada2492f952dedfe5f03efa49b4cfabf4
tianqiang.yang <ytq1070007954@gmail.com>
2025-11-01 20:28:58 +0800

    添加结算界面

--- Assets/Scripts/DataSystem_Map.cs
+++ Assets/Scripts/DataSystem_Map.cs
@@ -1,6 +1,17 @@
 public partial class DataSystem
 {
 	public static float InputExtraTime = 0.3f;
+
+	public static float BaseScore = 5000f;
+	public static float ItemBonus = 0.20f;
+	public static float DifficultyRate = 1.5f;
+
+	public static float CalculateFinalScore(int combo)
+	{
+		float comboBonus = combo * 0.1f;
+		return BaseScore * (1f + comboBonus + ItemBonus) * DifficultyRate;
+	}
+
 	public void LoadGameData()
 	{
 		LoadDataLevel();
