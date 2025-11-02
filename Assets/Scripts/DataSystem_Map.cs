318aedd31b50697ead0571e15348a0c757208fd7
Changlin Tian <2083580942@qq.com>
2025-11-02 12:05:15 +0800

    评分

--- Assets/Scripts/DataSystem_Map.cs
+++ Assets/Scripts/DataSystem_Map.cs
@@ -1,6 +1,15 @@
+public enum ERatingGrade
+{
+    D = 0,
+    C = 1,
+    B = 2,
+    A = 3,
+    S = 4
+}
+
 public partial class DataSystem
 {
-	public static float InputExtraTime = 0.3f;
+	public static float InputExtraTime = 0.5f; //按下阈值
 
 	public static float BaseScore = 5000f;
 	public static float ItemBonus = 0.20f;
@@ -11,6 +20,30 @@ public partial class DataSystem
 		float comboBonus = combo * 0.1f;
 		return BaseScore * (1f + comboBonus + ItemBonus) * DifficultyRate;
 	}
+
+	 private ERatingGrade GetRatingGrade(float accuracy)
+    {
+        if (accuracy >= 0.8f)
+        {
+            return ERatingGrade.S;
+        }
+        else if (accuracy >= 0.7f)
+        {
+            return ERatingGrade.A;
+        }
+        else if (accuracy >= 0.6f)
+        {
+            return ERatingGrade.B;
+        }
+        else if (accuracy >= 0.5f)
+        {
+            return ERatingGrade.C;
+        }
+        else
+        {
+            return ERatingGrade.D;
+        }
+    }
 
 	public void LoadGameData()
 	{
